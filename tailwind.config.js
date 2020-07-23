ed7f50c7e4d261905c3eb0beaf526480a427688b
Elijah <rahulsharma240301@gmail.com>
2020-07-23 14:44:41 +0530

    Complete 100%

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffa9a7',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
